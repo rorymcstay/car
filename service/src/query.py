33020b2938cb0b576d7d7530b9cde416ea887d3a
rorymcstay <rory@rorymcstay.com>
2018-12-11 23:10:31 +0000

    query

--- service/src/query.py
+++ service/src/query.py
@@ -1,38 +1,46 @@
 import json
-from flask import request, Blueprint
-from car.market.src.persisting.queryconstructor import QueryConstructor
-from car.service.src.headers import JSON
-from car.market.src.persisting.mongoservice import MongoService
+import yaml
+
+from bson import json_util
+from flask import request, Blueprint, json
+from flask_classy import FlaskView, route
+from car.market.src.mongo_service.QueryConstructor import QueryConstructor
+from car.flask_helper.headers import JSON
+from car.market.src.mongo_service.MongoService import MongoService
 
 query = Blueprint('query', __name__,)
 service = MongoService()
 
+class Query(FlaskView):
 
-@query.route("/cars")
-def getAllCars():
-    return json.dumps(service.queryToJSON({}))
+    @route("/cars")
+    def get_all_cars(self):
+        return json.dumps(service.query({}))
 
+    def filter(self):
+        """
+        :return: filters the cars collection by make, model and age
+        """
+        make = request.args.get('make', default='', type=str)
+        model = request.args.get('model', default='', type=str)
+        age = request.args.get('age', default='', type=int)
+        values = [make, model, age]
+        names = ['make', 'model', 'age']
+        used_params = []
+        used_names = []
+        for name, value in zip(names, values):
+            if value != '':
+                used_params.append(value)
+                used_names.append(name)
+        query = QueryConstructor(used_params, used_names, 'carDetails').main()
+        response = json.dumps(service.query(query))
+        return response
 
-@query.route('/cars/')
-@JSON
-def filter():
-    """
-    :return: filters the cars collection by make, model and age
-    """
-    make = request.args.get('make', default='', type=str)
-    model = request.args.get('model', default='', type=str)
-    age = request.args.get('age', default='', type=int)
-    values = [make, model, age]
-    names = ['make', 'model', 'age']
-    usedParams = []
-    usedNames = []
-    for name, value in zip(names, values):
-        if value != '':
-            usedParams.append(value)
-            usedNames.append(name)
-    query = QueryConstructor(usedParams, usedNames, 'carDetails').main()
-    response = json.dumps(service.queryToJSON(query))
-    return response
-
+    @route('/cars/')
+    @JSON
+    def json_query(self):
+        req = yaml.safe_load(json.dumps(request.get_json()))
+        x = service.cars.find(req['query'], req['projection'])
+        return json_util.dumps(x)
 
 
