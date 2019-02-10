70b5b7c044b5e6ad33e0857d1bc0e89abb9550d1
rorymcstay <rory@rorymcstay.com>
2019-02-10 00:52:08 +0000

    local docker app can control docker ec2 of browsers

--- aws_deployment/task_configurations/market_task.sh
+++ aws_deployment/task_configurations/market_task.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-ecs-cli compose --project-name market --file market-loader.yml --ecs-params loader-config.yml --region us-west-2 create --launch-type FARGATE
+ecs-cli compose --project-name car-app --file market-loader.yml --ecs-params loader-config.yml --region us-west-2 create --launch-type FARGATE
